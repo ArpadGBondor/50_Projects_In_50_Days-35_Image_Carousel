423e7614c4dffec77b2a433019f9fc88e3f5c9ce
Arpad G. Bondor <arpad.g.bondor@gmail.com>
2021-02-05 04:30:40 +0000

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,32 @@
+const imgs = document.getElementById('imgs');
+const left = document.getElementById('left');
+const right = document.getElementById('right');
+
+const img = document.querySelectorAll('#imgs img');
+
+left.addEventListener('click', prev);
+right.addEventListener('click', next);
+
+let index = 0;
+
+let interval = setInterval(next, 2000);
+
+function next() {
+  index++;
+  if (index >= img.length) {
+    index = 0;
+  }
+  changeImage();
+}
+
+function prev() {
+  index--;
+  if (index < 0) {
+    index = img.length - 1;
+  }
+  changeImage();
+}
+
+function changeImage() {
+  imgs.style.transform = `translateX(${index * -500}px)`;
+}
